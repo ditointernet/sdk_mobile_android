b14596845ef0c77f490e6c96ef47aa3e65a0692f
Felipe Arimateia <felipearimateia@gmail.com>
2018-10-18 01:05:31 -0300

    Sample in Kotlin

---
+++ SampleKotlin/app/src/main/java/br/com/dito/samplekotlin/DitoApplication.kt
@@ -0,0 +1,19 @@
+package br.com.dito.samplekotlin
+
+import android.app.Application
+import br.com.dito.ditosdk.BuildConfig
+import br.com.dito.ditosdk.Dito
+import com.facebook.stetho.Stetho
+
+
+class DitoApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
+        Dito.init(this, null)
+    }
+}

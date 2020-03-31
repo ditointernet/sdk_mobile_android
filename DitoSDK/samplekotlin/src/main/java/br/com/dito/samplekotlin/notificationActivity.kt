7eede773d88358a758d1da31af3c990b70356e00
Breno <breno.asrm@gmail.com>
2020-03-31 20:29:51 -0300

    add new activity to test deep link

---
+++ DitoSDK/samplekotlin/src/main/java/br/com/dito/samplekotlin/notificationActivity.kt
@@ -0,0 +1,12 @@
+package br.com.dito.samplekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class notificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification3)
+    }
+}
